1c7127527355ae49f38bdaa9500910680db96495
joaosquincali <110679269+joaosquincali@users.noreply.github.com>
2024-08-02 07:46:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado'); 
+
+const perguntas = [
+
+   
+    
+]
