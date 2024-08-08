cf65794bf25acfcc607d64233e685280ff275dd0
joaosquincali <110679269+joaosquincali@users.noreply.github.com>
2024-08-08 08:32:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
-const caixaResultado = document.querySelector('.caixa-resultado'); 
+const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [
-
-   
-    
+    {
+        enunciado:"Pergunta 1",
+        alternativas:[ "Alternativa 1", "Alternativa 2" ]
+    },
 ]
+  
