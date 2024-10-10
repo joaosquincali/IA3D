25c28dbe2f7c28935e8cd3dd2017d84fc70a699d
joaosquincali <110679269+joaosquincali@users.noreply.github.com>
2024-10-10 08:36:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
-        enunciado: "A inteligência artificial é útil?",
+        enunciado: "A Inteligência Artificial é útil?",
         alternativas: [
             {
                 texto: "Sim",
@@ -50,8 +51,13 @@ let perguntaAtual;
 let respostas = "";
 
 function mostraPergunta() {
+    if (posicao >= perguntas.length){
+        mostraResultado();
+        return;
+        }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
 function mostraAlternativas() {
@@ -64,8 +70,13 @@ function mostraAlternativas() {
 }
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmativa;
-    respostas = afirmacoes;
+    respostas += afirmacoes;
     posicao++;
     mostraPergunta();
+}
+function mostraResultado(){
+    caixaPergunta.textContent = "Em 25 anos...";
+    textoResultado.textContent = respostas; 
+    caixaAlternativa.textContent = "";
 }
 mostraPergunta();
